51cca5896cf8f17eb8c51d935c4511bbfbc42cab
Get-Bad <92388856+Get-Bad@users.noreply.github.com>
2022-07-20 17:52:12 +0300

    First commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculator
+{
+    public static class Calculator
+    {
+        private static void Main(string[] args)
+        {
+            int sum1;
+            int sum2;
+            String mathOperator;
+
+            Console.WriteLine("Enter first number: ");
+            sum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter math operator: ");
+            mathOperator = Console.ReadLine();
+
+            Console.WriteLine("Enter second number: ");
+            sum2 = Convert.ToInt32(Console.ReadLine());
+
+            switch(mathOperator)
+            {
+                case "+":
+                    int sumAnswer = sum1 + sum2;
+                    Console.WriteLine("Answer: " + sumAnswer);
+                    break;
+                
+                case "-":
+                    sumAnswer = sum1 - sum2;
+                    Console.WriteLine("Answer: " + sumAnswer);
+                    break;
+
+                case "*":
+                    sumAnswer = sum1 * sum2;
+                    Console.WriteLine("Answer: " + sumAnswer);
+                    break;
+
+                case "/":
+                    sumAnswer = sum1 / sum2;
+                    Console.WriteLine("Answer: " + sumAnswer);
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown value");
+                    break;
+            }
+        }
+    }
+}
